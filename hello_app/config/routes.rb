87d6485f085779332ee04d5009f78d151f0d843d
Viktorkosandyak <kosandyak1996@gmail.com>
2018-05-18 15:01:56 +0300

    hhh

---
+++ hello_app/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # Определить корневой маршрут к сайту можно как "root"
+  root 'application#hello'
+end
